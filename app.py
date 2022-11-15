42a8b6583a6bd7027482c9bb3681b779f8919529
Paul Fletes <paul.fletes@gmail.com>
2022-11-15 16:49:58 -0600

    commit challenge 3

--- app.py
+++ app.py
@@ -162,21 +162,27 @@ def image_filter():
         # as a variable
         # HINT: remember that we're working with a POST route here so which requests function would you use?
         filter_type = ''
+        filter_type = request.form.get('filter_types')
 
         # Get the image file submitted by the user
         image = request.files.get('users_image')
 
         # TODO: call `save_image()` on the image & the user's chosen filter type, save the returned
         # value as the new file path
+        image_path = save_image(image, filter_type)
 
         # TODO: Call `apply_filter()` on the file path & filter type
 
         image_url = f'./static/images/{image.filename}'
+        apply_filter(image_path, filter_type)
 
         context = {
             # TODO: Add context variables here for:
             # - The full list of filter types
+            'filter_types': filter_types,
+            'filter_type': filter_type,
             # - The image URL
+            'image_url': image_url,
         }
 
         return render_template('image_filter.html', **context)
@@ -184,6 +190,7 @@ def image_filter():
     else:  # if it's a GET request
         context = {
             # TODO: Add context variable here for the full list of filter types
+            'filter_types': filter_types,
         }
         return render_template('image_filter.html', **context)
 
